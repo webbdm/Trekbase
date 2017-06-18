1dec9891da0501f73965a809cb01564939cf7795
webbdm <gwebb762@gmail.com>
2017-06-17 22:14:15 -0500

    CSS

--- javascript/controllers/NavCtrl.js
+++ javascript/controllers/NavCtrl.js
@@ -1,4 +1,15 @@
-app.controller("NavCtrl", function($scope, $rootScope) {
+app.controller("NavCtrl", function($location, $scope, $rootScope) {
+
+	$scope.onHomepage = true;
+	$scope.currentLocation = $location.path();
+	console.log($scope.currentLocation);
+
+	if($scope.currentLocation !== "/auth"){
+
+		$scope.onHomepage = false;
+		console.log("Not Homepage!");
+
+	}
 
 	$scope.text = "Capstone Project";
 
