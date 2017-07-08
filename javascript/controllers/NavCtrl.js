70756d01ef947a456117c6ddc6eeec8710b7300e
webbdm <gwebb762@gmail.com>
2017-07-08 13:00:02 -0500

    Styled the form inputs

--- javascript/controllers/NavCtrl.js
+++ javascript/controllers/NavCtrl.js
@@ -1,18 +1,5 @@
 app.controller("NavCtrl", function($location, $scope, $rootScope) {
 
     $rootScope.tallNav = true;
-    // //$scope.currentLocation = $location.path();
-    // //$scope.homepage = true;
-
-    // $scope.$apply(() => {
-    //     $scope.homepage = $rootScope.tallNav;
-    // });
-
-    // if ($rootScope.tallNav === true) {
-    //     console.log("Tall Navbar");
-    // } else if ($rootScope.tallNav === false) {
-    //     $scope.homepage = false;
-    //     console.log("Short Navbar");
-    // }
 
 });
