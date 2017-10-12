31cb893d4b885f0d6e12fadfaad82ba27c504b41
webbdm <gwebb762@gmail.com>
2017-10-12 01:14:54 -0500

    Changed fonts

--- javascript/controllers/NavCtrl.js
+++ javascript/controllers/NavCtrl.js
@@ -1,5 +1,11 @@
 app.controller("NavCtrl", function($location, $scope, $rootScope) {
 
+
+    // Initialize collapse button
+    $(document).ready(function () {
+        $(".button-collapse").sideNav();
+    });
+
     $rootScope.tallNav = true;
 
 });
